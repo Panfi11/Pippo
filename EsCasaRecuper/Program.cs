13636eadc2a91df331ff38af032218d58e9da234
Pier Andrea Frontani <pierandreapaf@gmail.com>
2020-03-08 14:45:46 +0100

    Aggiungere i file di progetto.

---
+++ EsCasaRecuper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EsCasaRecuper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un numero positivo. ");
+            int a = int.Parse(Console.ReadLine());
+            if(a <= 0)
+            {
+                Console.WriteLine("Il numero non è positivo.");
+            }
+            else
+            {
+                if (a % 2 == 0)
+                {
+                    for (int i = 0; i <= a; i++)
+                    {
+                        Console.WriteLine($"{i}");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i <= a; i++)
+                    {
+                        Console.WriteLine($"{i}") ;
+
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
